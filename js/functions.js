718359b78e990b197978142448012f1f27dd2fd4
Ricardo <ricardo_caceres1995@hotmail.com>
2019-03-28 20:41:41 -0600

    Miio Web 1.0.9 (Sliders)

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,12 @@
-
-
 $("#show").click(function() {
   let width = $("#mySidenav").width()
   let total = (width > 0) ? 0 : 250
   $("#show").toggleClass("change");
-  $("#mySidenav").width(total.toString(), "px");
+  $("#mySidenav").animate({width:total.toString()+"px"},"fast");
 });
+//
 
+  // $("#mySidenav").width(total.toString(), "px");
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -23,62 +23,52 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
 
-var slideHover = 1;
-showSlides(slideHover);
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideHover += n);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideHover = n);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideHover = 1
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  if (n < 1) {
-    slideHover = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideHover - 1].style.display = "block";
-  dots[slideHover - 1].className += " active";
 }
-
 
 
 
 // EMPIEZA SLIDER 2 Y 3
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-// var slideIndex = [1,1];
-// /* Class the members of each slideshow group with different CSS classes */
-// var slideId = ["mySlides1", "mySlides2"]
-// showSlides(1, 0);
-// showSlides(1, 1);
-//
-// function plusSlides(n, no) {
-//   showSlides(slideIndex[no] += n, no);
-// }
-//
-// function showSlides(n, no) {
-//   var i;
-//   var x = document.getElementsByClassName(slideId[no]);
-//   if (n > x.length) {slideIndex[no] = 1}
-//   if (n < 1) {slideIndex[no] = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[slideIndex[no]-1].style.display = "block";
-// }
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "flex";
+}
