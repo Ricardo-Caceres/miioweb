5872ace520c4d3a00d7a781eb5bb638726830e1a
Ricardo <ricardo_caceres1995@hotmail.com>
2019-04-17 14:03:27 -0500

    Miio Web 1.1.6

---
+++ enviar.php
@@ -0,0 +1,60 @@
+
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+
+if(isset($_POST) && isset($_POST['nombre']) && isset($_POST['asunto']) && isset($_POST['correo']) && isset($_POST['comentarios'])) {
+  $nombre       = $_POST['nombre'];
+  $asunto       = $_POST['asunto'];
+  $correo       = $_POST['correo'];
+  $comentarios  = $_POST['comentarios'];
+
+  $emailTo      = "hola@miio.mx";  //<-- este es el correo a donde lo vas a enviar, en este caso la cuenta donde lo vas a recibir
+  $nameTo       = "Operaciones Miio";             // <-- el nombre de quien recibe el correo...
+
+  // echo gettype($nombre) . " " . $nombre . "\n";
+  // echo gettype($asunto) . " " . $asunto . "\n";
+  // echo gettype($correo) . " " . $correo .  "\n";
+  // echo gettype($comentarios) . " " . $comentarios . "\n";
+
+  $API_KEY = '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';
+
+  $email = new \SendGrid\Mail\Mail();
+  $email->setFrom($correo, $nombre);
+  $email->setSubject($asunto);
+  $email->addTo($emailTo, $nameTo);
+  $email->addContent("text/plain",$comentarios); // <-- este campo no sé qué pedo porque no se manda al correo
+  $email->addContent(
+      "text/html", "<strong>" . $comentarios . " </strong>"
+  ); // <-- AMBOS SON OBLIGATORIOS!!!
+
+  $sendgrid = new \SendGrid($API_KEY);
+
+
+  try {
+      $response = $sendgrid->send($email);
+      // echo $response->statusCode() . "\n";
+      echo ($response->statusCode() == 200 || $response->statusCode() == 201 || $response->statusCode() == 202) ? true : false;
+        // print $response->statusCode() . "puta \n";
+        // print_r($response->headers()) . "\n";
+        // print $response->body() . "ñaka \n";
+  } catch (Exception $e) {
+      echo 'Caught exception: '. $e->getMessage() ."\n";
+  }
+
+} else {
+  echo 'Faltan campos por enviar';
+}
+
+?>
--- js/functions.js
+++ js/functions.js
@@ -1,84 +1,122 @@
-$("#show").click(function() {
-  let width = $("#mySidenav").width()
-  let total = (width > 0) ? 0 : 250
-  $("#show").toggleClass("change");
-  $("#mySidenav").animate({width:total.toString()+"px"},"fast");
-});
-//
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-  // $("#mySidenav").width(total.toString(), "px");
+    const enviar = ()  =>  {
+      const nombre      = $('#nombre').val(),
+            asunto      = $('#asunto').val(),
+            correo      = $('#email').val(),
+            comentarios = $('#comentarios').val()
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-$('.close').click(function() {
-      modal.style.display = "none";
-      $('#modal-video').each(function(){
-        var el_src = $(this).attr("src");
-        $(this).attr("src",el_src);
+      var data = new Object({nombre,asunto,correo,comentarios})
+      // console.log(nombre);
+      // console.log(asunto);
+      // console.log(correo);
+      // console.log(comentarios);
+      request = $.ajax({
+          url: "enviar.php",
+          type: "POST",
+          dataType: 'json',
+          crossDomain: true,
+          data: data
       });
-});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    $('#modal-video').each(function(){
-      var el_src = $(this).attr("src");
-      $(this).attr("src",el_src);
+      request.done((data) =>  {
+        const res = (data == 1)
+                  ? 'El correo ha sido enviado satisfactoriamente'
+                  : 'Hubo un error al mandar el correo';
+        console.log(res);
+        alert(res)
+      })
+
+      request.fail((error) => {
+        alert( "aquí metes un card con fadeIn si hay error para que se vea bonito. El mmensaje lo recibess en la variable 'error'");
+      })
+    }
+
+    $("#submit").click(function(e) {
+       e.preventDefault();
+       enviar()
     });
-  }
-}
+
+    $("#show").click(function() {
+      let width = $("#mySidenav").width()
+      let total = (width > 0) ? 0 : 250
+      $("#show").toggleClass("change");
+      $("#mySidenav").animate({width:total.toString()+"px"},"fast");
+    });
+    //
+
+      // $("#mySidenav").width(total.toString(), "px");
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    $('#myBtn').click(()  =>  {
+      if(modal && modal.style && modal.style.display) modal.style.display = "block";
+    })
+
+    // When the user clicks on <span> (x), close the modal
+    $('.close').click(function() {
+          modal.style.display = "none";
+          $('#modal-video').each(function(){
+            var el_src = $(this).attr("src");
+            $(this).attr("src",el_src);
+          });
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+        $('#modal-video').each(function(){
+          var el_src = $(this).attr("src");
+          $(this).attr("src",el_src);
+        });
+      }
+    }
 
 
-// EMPIEZA SLIDER 2 Y 3
-var slideIndex = [1,1];
-/* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+    // EMPIEZA SLIDER 2 Y 3
+    var slideIndex = [1,1];
+    /* Class the members of each slideshow group with different CSS classes */
+    var slideId = ["mySlides1", "mySlides2"]
+    showSlides(1, 0);
+    showSlides(1, 1);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
+    function plusSlides(n, no) {
+      showSlides(slideIndex[no] += n, no);
+    }
 
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "flex";
-}
-
-
-$("#submit").click(function() {
-alert("¡Muchas gracias por tu mensaje! Te daremos una respuesta en breve.");
+    function showSlides(n, no) {
+      var i;
+      var x = document.getElementsByClassName(slideId[no]);
+      if (n > x.length) {slideIndex[no] = 1}
+      if (n < 1) {slideIndex[no] = x.length}
+      for (i = 0; i < x.length; i++) {
+        if(x[i] && x[i].style && x[i].style.display) x[i].style.display = "none";
+      }
+      if(x[slideIndex[no]-1] && x[slideIndex[no]-1].style && x[slideIndex[no]-1].style.display) x[slideIndex[no]-1].style.display = "flex";
+    }
 });
